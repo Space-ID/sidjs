14eaec34a9b478070dd280000a4d8428e1b6814d
Hui Zhao <98204090+huihzhao@users.noreply.github.com>
2022-08-26 08:35:41 +0200

    Update rpc.js

--- src/constants/rpc.js
+++ src/constants/rpc.js
@@ -1,6 +1,4 @@
 export const apis = {
-    bsc_mainnet: "https://bsc-mainnet.nodereal.io/v1/c4bc3afc2f804d4ea74fafb295828c97",
-    bsc_testnet: "https://bsc-testnet.nodereal.io/v1/c4e97743e5674252bb0f7f7b6d4b933e",
-    eth_mainnet: "https://eth-mainnet.nodereal.io/v1/bb727c94ca474d7ea1185724fdc87a49"
+    bsc_mainnet: "https://bsc-mainnet.nodereal.io/v1/d0c3ef1cdb0247f4b6fae228aa76c8b8"
 } 
 
