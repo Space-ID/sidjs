b75dce458babef296ded4ca2141668d1869da1f7
kevin-222 <110803206+kevin-222@users.noreply.github.com>
2023-05-15 16:08:00 +0800

    Feat/support ccip (#24)
    * feat: support ccip
    
    * v0.1.24

--- src/constants/whitelist.js
+++ src/constants/whitelist.js
@@ -1,3 +1,1 @@
-export default [
-    'cz.bnb', 'id.bnb', 'sm.bnb', 'yg.bnb', 'go.arb'
-]
+export default ['cz.bnb', 'id.bnb', 'sm.bnb', 'yg.bnb', 'cz.eth.bnb', 'id.eth.bnb', 'sm.eth.bnb', 'yg.eth.bnb', 'go.arb']
